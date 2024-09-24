db98f52911f2d46bb80d51b17c8bb35803d231c7
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-09-24 09:30:24 -0400

    part1 completed

---
+++ main.js
@@ -0,0 +1,27 @@
+
+// PART 1: Fizz Buzz
+// loopd through all numbers from 1- 100
+// for (let i=0; i<=100; i++){
+//     console.log(i)
+// }
+
+// for (let i=0; i<=100; i++) {
+//     if(i % 3==0)
+//         console.log('Fizz')
+// }
+
+// for (let i=0; i<=100; i++) {
+//     if(i % 5==0)
+//         console.log('Buzz')
+// }
+
+// for (let i=0; i<=100; i++) {
+//     if(i % 3==0 && i % 5 == 0)
+//         console.log('Fizz Buzz')
+// }
+
+// for (let i=0; i<=100; i++) {
+//     if (!(i % 3==0 || i % 5==0))
+//         console.log(i)
+// }
+
