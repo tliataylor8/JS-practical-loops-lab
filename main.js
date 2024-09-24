50c8c22cbb14f0d0868dece916b48901cf2c1a6d
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-09-24 11:47:53 -0400

    P3 done, check P2

--- main.js
+++ main.js
@@ -39,14 +39,11 @@
 
 // 
 
-// PART 3: Feeling Loopy
-// let milkShakeSpots = ['Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232']
+// // PART 3: Feeling Loopy
+// let csvString = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
+// let sepArray = csvString.split(',');
+// console.log(sepArray);
 
-// // While loops
-// let i = 0;
-// while(i<10){
-//     console.log(i);
-
-//     i++;
-// }
+// for(let i=0; i<sepArray.length; i++) {
+//     console.log(sepArray);
 // }
