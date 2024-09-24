418f0086fe782d62a049b9aee76869a321cc3bfa
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-09-24 11:08:43 -0400

    part 2+3

--- main.js
+++ main.js
@@ -25,3 +25,28 @@
 //         console.log(i)
 // }
 
+// PART 2: PRIME TIME **REVIEW
+// //
+// for (let i=0; i<=100; i++) {
+//     if(i % 1==0 || i % i==0)
+//         console.log(i)
+// }
+// // 
+// for (let n=1; n<=100; n++) {
+//     if(Math.sqrt(n)==0)
+//         console.log(n)
+// }
+
+// 
+
+// PART 3: Feeling Loopy
+// let milkShakeSpots = ['Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232']
+
+// // While loops
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+
+//     i++;
+// }
+// }
